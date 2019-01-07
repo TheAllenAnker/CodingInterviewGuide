fdaea45839f158c449bdd50649c77c8e59428474
TheAllenAnker <theallenanker@gmail.com>
2019-01-07 14:08:23 +0800

    Solved problem: Min Stack(a stack that implements getMin() in O(1) time complexity).

--- src/com/allen_anker/chapter1_stackandqueue/MinStack.java
+++ src/com/allen_anker/chapter1_stackandqueue/MinStack.java
@@ -1,5 +1,41 @@
 package com.allen_anker.chapter1_stackandqueue;
 
-public class MinStack {
+import java.util.Stack;
 
+public class MinStack {
+    private Stack<Integer> dataStack;
+    private Stack<Integer> minStack;
+
+    public MinStack() {
+        dataStack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int num) {
+        dataStack.push(num);
+        if (minStack.isEmpty() || minStack.peek() > num) {
+            minStack.push(num);
+        } else {
+            minStack.push(minStack.peek());
+        }
+    }
+
+    public int pop() {
+        minStack.pop();
+        return dataStack.pop();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+
+    public static void main(String[] args) {
+        MinStack stack = new MinStack();
+        stack.push(2);
+        stack.push(3);
+        stack.push(1);
+        System.out.println(stack.getMin());
+        stack.pop();
+        System.out.println(stack.getMin());
+    }
 }
